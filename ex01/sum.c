78df5e7d3fa07394a7fbb121e0d87f6fa88c3758
Vladimir Still <vl.still@gmail.com>
2014-01-15 08:36:28 +0100

    ex01: Fix warnings in sum.c, use slightly safer way of abusing type system.

--- ex01/sum.c
+++ ex01/sum.c
@@ -3,11 +3,13 @@
 
 #include <pthread.h>
 #include <stdio.h>
+#include <stdint.h>
 
 int i = 0;
 
 // Note the return type: void*
-void* adder(int step){
+void* adder(void* _step){
+    int step = (int)((intptr_t)(_step));
     for(int x = 0; x < 1000000; x++){
         i += step;
     }
@@ -17,8 +19,8 @@ void* adder(int step){
 int main(){
     pthread_t adder_thr;
     pthread_t subst_thr;
-    pthread_create(&adder_thr, NULL, adder, 1);
-    pthread_create(&subst_thr, NULL, adder, -1);
+    pthread_create(&adder_thr, NULL, adder, (void*)(1) );
+    pthread_create(&subst_thr, NULL, adder, (void*)(-1) );
     pthread_join(adder_thr, NULL);
     pthread_join(subst_thr, NULL);
     printf("%i\n", i);
