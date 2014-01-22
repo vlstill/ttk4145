a19e9e59485b5a81f5241b35dbb84e66747c772f
Vladimir Still <vl.still@gmail.com>
2014-01-22 09:10:06 +0100

    ex03: Update.

--- ex03/network.go
+++ ex03/network.go
@@ -1,12 +1,23 @@
 package main
 
 import (
-    . "fmt"
-    . "net"
-    . "runtime"
+    "fmt"
+    "net"
+    "runtime"
+    "bufio"
 )
 
-func main() {
-    GOMAXPROCS(NumCPU())
+func conTCP() {
+    conn, err := net.Dial("tcp", "google.com:80")
+    if err != nil {
+      // handle error
+    }
+    fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n")
+    status, err := bufio.NewReader(conn).ReadString('\000')
+    fmt.PrintLn( status )
+}
 
+func main() {
+    runtime.GOMAXPROCS( runtime.NumCPU() )
+    conTCP()
 }
