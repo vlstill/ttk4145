874a1a9c315705edb1b520dd29b1ff9aa7791e05
Martin Ukrop <mukrop@mail.muni.cz>
2014-01-22 11:38:54 +0100

    ex03: tcp excercise

--- ex03/network.go
+++ ex03/network.go
@@ -5,19 +5,109 @@ import (
     "net"
     "runtime"
     "bufio"
+    "os"
+    "strconv"
+    "sync"
+    "time"
 )
 
-func conTCP() {
-    conn, err := net.Dial("tcp", "google.com:80")
-    if err != nil {
-      // handle error
+const server = "129.241.187.161"
+const tcpPortFixed = "34933"
+const tcpPortDelim = "33546"
+const tcpPortListening = "2222"
+
+// #### FIXED TCP READER
+
+func readAndPrintFixed(reader *bufio.Reader) {
+    str := make([]byte, 1024)
+    
+    n, err := reader.Read(str)
+    if n != 1024 || err != nil {
+        fmt.Fprintln(os.Stderr, "read size: " + strconv.Itoa(n))
+        fmt.Fprintln(os.Stderr, "Fixed reader error: " + err.Error())
+        return
     }
-    fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n")
-    status, err := bufio.NewReader(conn).ReadString('\000')
-    fmt.PrintLn( status )
+    fmt.Println(string(str[:n]))
 }
 
+// #### 0-DELIMITED TCP READER
+
+func readAndPrintDelim(reader *bufio.Reader) {
+    str, err := reader.ReadString('\000')
+    if err != nil {
+        fmt.Fprintln(os.Stderr, "Delim reader error: " + err.Error())
+        return
+    }
+    fmt.Println("received: " + str)
+}
+
+// #### TCP CONNECTOR
+
+func conTCP(port string, callback func(*bufio.Reader)) {
+    conn, err := net.Dial("tcp", net.JoinHostPort(server, port))
+    if err != nil {
+        fmt.Fprintln(os.Stderr, "connection error on " + server + ":" + port)
+        fmt.Fprintln(os.Stderr, "Connection join error: " + err.Error())
+        return
+    }
+    defer conn.Close()
+    
+    // chat withe server :-}
+    wg.Add(1)
+    chatWithServer(conn, callback)
+    
+    // initialize socket for listening
+    addr, err := net.ResolveTCPAddr("tcp", ":"+tcpPortListening)
+    if err != nil {
+        fmt.Fprintln(os.Stderr, "Address resolution error: " + err.Error())
+    }
+    ln, err := net.ListenTCP("tcp", addr)
+    if err != nil {
+        fmt.Fprintln(os.Stderr, "Connection init error: " + err.Error())
+    }
+    ln.SetDeadline(time.Now().Add(2*time.Second))
+    defer ln.Close()
+    // ask for 2 reverse connection
+    fmt.Fprintf(conn, "Connect to: 129.241.187.149:" + tcpPortListening + "\000")
+    fmt.Fprintf(conn, "Connect to: 129.241.187.149:" + tcpPortListening + "\000")
+    // handle incoming connections
+    deadline := time.Now().Add(2*time.Second)
+    for time.Now().Before(deadline) {
+        conn2, err := ln.Accept()
+        if err != nil {
+            fmt.Fprintln(os.Stderr, "Socket acceptance error: " + err.Error())
+            continue
+        }
+        defer conn2.Close()
+        wg.Add(1)
+        go chatWithServer(conn2, callback)
+    }
+    wg.Wait();
+    
+}
+
+// #### server CHATTER
+
+func chatWithServer(conn net.Conn, callback func(*bufio.Reader)) {
+    reader := bufio.NewReaderSize(conn, 1024)
+    // read welcome message
+    callback(reader)
+    // chat for a while
+    for i := 0; i < 5; i++ {
+        fmt.Fprintf(conn, "Hello world: " + strconv.Itoa(i) + "\000")
+        callback(reader)
+    }
+    wg.Done()
+}
+
+// #### MAIN
+var wg sync.WaitGroup
+
 func main() {
-    runtime.GOMAXPROCS( runtime.NumCPU() )
-    conTCP()
+    runtime.GOMAXPROCS(runtime.NumCPU())
+    fmt.Println("=== Fixed TCP communication ===");
+    conTCP(tcpPortFixed, readAndPrintFixed)
+    fmt.Println()
+    fmt.Println("=== 0-delimited TCP communication ===");
+    conTCP(tcpPortDelim, readAndPrintDelim)
 }
