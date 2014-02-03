645c521a70c3d06118c93002fa7dd0af103e1524
Vladimir Still <vl.still@gmail.com>
2014-02-03 13:02:59 +0100

    project: Add (prototype of) concurrent queue.

---
+++ project/src/concurrentqueue.h
@@ -0,0 +1,47 @@
+#include <mutex>
+#include <deque>
+
+#ifndef SRC_CONCURRENT_QUEUE_H
+#define SRC_CONCURRENT_QUEUE_H
+
+/* Concurrent queue
+ * so far just make sure it is consisten, no performance optimizations
+ * but interface should be general enought to replace queue with faster one
+ */
+
+template< typename T >
+struct ConcurrentQueue {
+
+    void enqueue( const T &data ) {
+        Guard g{ _lock };
+        _queue.push_back( data );
+        g.unlock();
+        _cond.notify_one();
+    }
+
+    /** get and pop head of queue, this will block if queue is empty
+     */
+    T dequeue() {
+        Guard g{ _lock };
+        while ( _queue.empty() )
+            _cond.wait( g );
+        T data = _queue.front();
+        _queue.pop_front();
+        return data;
+    }
+
+    /** not safe
+     */
+    bool empty() {
+        Guard g{ _lock };
+        return _queue.empty();
+    }
+
+  private:
+    std::mutex _lock;
+    std::deque< T > _queue;
+    std::condition_variable _cond;
+    using Guard = std::unique_lock< std::mutex >;
+};
+
+#endif // SRC_CONCURRENT_QUEUE_H
