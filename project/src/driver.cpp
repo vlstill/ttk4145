6128bcd4a9b5371ad25426d0a75c5ff4254872cd
Vladimir Still <vl.still@gmail.com>
2014-02-09 10:37:46 +0100

    driver: Fix buch of warnings.

--- project/src/driver.cpp
+++ project/src/driver.cpp
@@ -10,28 +10,28 @@ static const int N_FLOORS = 4;
 static const int N_BUTTONS = 3;
 
 static const std::array< std::array< int, N_BUTTONS >, N_FLOORS > lampChannelMatrix{ {
-    {LIGHT_UP1, LIGHT_DOWN1, LIGHT_COMMAND1},
-    {LIGHT_UP2, LIGHT_DOWN2, LIGHT_COMMAND2},
-    {LIGHT_UP3, LIGHT_DOWN3, LIGHT_COMMAND3},
-    {LIGHT_UP4, LIGHT_DOWN4, LIGHT_COMMAND4},
+    { { LIGHT_UP1, LIGHT_DOWN1, LIGHT_COMMAND1 } },
+    { { LIGHT_UP2, LIGHT_DOWN2, LIGHT_COMMAND2 } },
+    { { LIGHT_UP3, LIGHT_DOWN3, LIGHT_COMMAND3 } },
+    { { LIGHT_UP4, LIGHT_DOWN4, LIGHT_COMMAND4 } },
 } };
 
 static const std::array< std::array< int, N_BUTTONS >, N_FLOORS > buttonChannelMatrix{ {
-    {FLOOR_UP1, FLOOR_DOWN1, FLOOR_COMMAND1},
-    {FLOOR_UP2, FLOOR_DOWN2, FLOOR_COMMAND2},
-    {FLOOR_UP3, FLOOR_DOWN3, FLOOR_COMMAND3},
-    {FLOOR_UP4, FLOOR_DOWN4, FLOOR_COMMAND4},
+    { { FLOOR_UP1, FLOOR_DOWN1, FLOOR_COMMAND1 } },
+    { { FLOOR_UP2, FLOOR_DOWN2, FLOOR_COMMAND2 } },
+    { { FLOOR_UP3, FLOOR_DOWN3, FLOOR_COMMAND3 } },
+    { { FLOOR_UP4, FLOOR_DOWN4, FLOOR_COMMAND4 } },
 } };
 
 int lamp( Button btn ) {
-    assert_leq( btn.floor(), lampChannelMatrix.size(), "out-of-bounds" );
+    assert_leq( btn.floor(), int( lampChannelMatrix.size() ), "out-of-bounds" );
     assert_leq( 1, btn.floor(), "out-of-bounds" );
 
     return lampChannelMatrix[ btn.floor() - 1 ][ int( btn.type() ) ];
 }
 
 int button( Button btn ) {
-    assert_leq( btn.floor(), buttonChannelMatrix.size(), "out-of-bounds" );
+    assert_leq( btn.floor(), int( buttonChannelMatrix.size() ), "out-of-bounds" );
     assert_leq( 1, btn.floor(), "out-of-bounds" );
 
     return buttonChannelMatrix[ btn.floor() - 1 ][ int( btn.type() ) ];
