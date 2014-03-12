987c89caefe719cfac55f35e4abf347ffa266738
Vladimir Still <vl.still@gmail.com>
2014-03-12 20:11:11 +0100

    heartbeat: Add heartbeat manager.

--- project/src/heartbeat.h
+++ project/src/heartbeat.h
@@ -1,8 +1,11 @@
+#include <src/test.h>
+#include <thread>
 #include <chrono>
 #include <cstdint>
 #include <atomic>
 #include <exception>
 #include <string>
+#include <deque>
 
 #ifndef SRC_HEARTBEAT_H
 #define SRC_HEARTBEAT_H
@@ -33,6 +36,7 @@ struct HeartBeatException : std::exception {
 struct HeartBeat {
 
     HeartBeat( int64_t threshold ) : _threshold( threshold ) { }
+    HeartBeat( const HeartBeat & ) = delete; // disable copying
 
     static int64_t now() {
         return std::chrono::duration_cast< std::chrono::milliseconds >(
@@ -61,6 +65,58 @@ struct HeartBeat {
         int64_t lastBeat = _lastBeat.load( std::memory_order_acquire );
         return now() - lastBeat;
     }
+};
+
+struct HeartBeatManager {
+    HeartBeatManager() : terminate( false ), rerunTime( 10000 ) { }
+    ~HeartBeatManager() {
+        if ( thr.joinable() ) {
+            terminate = true;
+            thr.join();
+        }
+    }
+
+    /* creates new HeartBeat objects and sets rerun time
+     * to at least 10 times shorter period that is threshold
+     * of this beat
+     */
+    HeartBeat &getNew( int64_t threshold ) {
+        int64_t thisRerun = threshold / 10;
+        if ( rerunTime > thisRerun )
+            rerunTime = thisRerun;
+        beats.emplace_back( threshold );
+        return beats.back();
+    }
+
+    /* Runs thread running checking loop in background,
+     * this function can be run at most once
+     * thread will stop when HeartBeatManager is destructed
+     * this loop runs every rerunTime milliseconds (+ scheduler delay)
+     * and checks validity of all heartbeats
+     * it heartbeat fails it will throw HeartBeatException which will
+     * terminate program if uncought
+     */
+    void run() {
+        assert( !thr.joinable(), "attempt to re-run HeartBeatManager's check thread" );
+        thr = std::thread( &HeartBeatManager::checkLoop, this );
+    }
+
+  private:
+    void checkLoop() {
+        while ( !terminate.load( std::memory_order::memory_order_relaxed ) ) {
+            auto next = std::chrono::steady_clock::now() + std::chrono::milliseconds( rerunTime );
+
+            for ( auto &b : beats )
+                b.throwIfLate();
+
+            std::this_thread::sleep_until( next );
+        }
+    }
+
+    std::deque< HeartBeat > beats;
+    std::thread thr;
+    std::atomic< bool > terminate;
+    int64_t rerunTime;
 };
 
 }
