ce226ef0c4fa34d2b33147ee35b0eaf4bb0508f2
Sameh Khalil <samehk@stud.ntnu.no>
2014-03-15 13:58:32 +0100

    main: Prototype of initialization.

--- project/tools/main.cpp
+++ project/tools/main.cpp
@@ -3,19 +3,31 @@
 #include <elevator/scheduler.h>
 #include <wibble/commandline/parser.h>
 #include <iostream>
+#include <elevator/udptools.h>
+#include <atomic>
+#include <thread>
+#include <chrono>
+#include <set>
 
 namespace elevator {
 
 using namespace wibble;
 using namespace wibble::commandline;
+using namespace udp;
 
 struct Main {
 
+	const Address sendAdd{ IPv4Address::localhost, Port{ 64123 } };
+	const Address rcvAdd{ IPv4Address::localhost, Port{ 64125 } };
+	const Address brdcastAdd{ IPv4Address::broadcast, Port{ 64125 } };
     StandardParser opts;
     OptionGroup *execution;
     IntOption *nodes;
     BoolOption *avoidRecovery;
     IntOption *elevatorId;
+	const int peerMsg = 1000;
+	std::set<IPv4Address> peerAddresses;
+	int id = INT_MIN;
 
     Main( int argc, const char **argv ) : opts( "elevator", "0.1" ) {
         // setup options
@@ -51,8 +63,54 @@ struct Main {
             exit( 0 );
     }
 
+	void sendToPeers( atomic <bool> *trmntSend )
+	{
+		while( !*trmntSend ) {
+			Socket snd_sock{ sendAdd, true };
+			Packet pack( sizeof( int ) );
+			pack.address() = brdcastAdd;
+			pack.get<int>() = peerMsg;
+			snd_sock.sendPacket( pack );
+			std::this_thread::sleep_for( std::chrono::seconds( 0.5 ) );
+		}
+	}
+
+	void  findPeersListener()
+	{
+		Socket rd_sock{ rcvAdd, true };
+		//printf( "Starting Listener\n" );
+		while( true ) {
+			Packet pack = rd_sock.recvPacketWithTimeout( 300 );
+            if ( pack.size() != 0 )
+			{
+				//printf( "Received Packet is: %d \n", pack.get< int >() );
+				if( pack.get< int >() == peerMsg )
+				{
+					peerAddresses.insert( pack.address() );
+					if( peerAddresses.size() == 3)
+						trmntSend = true;
+				}
+			}
+		}
+	}
+
     void main() {
-        int id = INT_MIN;
+        
+		std::atomic<bool> trmntSend (false);
+		std::thread findPeers(sendToPeers, this, &trmntSend);
+		findPeersListener();
+		findPeers.join();
+
+		Socket m_sock{ sendAdd, true }
+		
+		int i = 0;
+		for( auto address : peerAddresses )
+		{
+			Packet pack;
+			pack.address() = brdcastAdd;
+			pack.get<int>() = i;
+			i++;
+		}
         if ( elevatorId->boolValue() ) {
             id = elevatorId->intValue();
         } else {
