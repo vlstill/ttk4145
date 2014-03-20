92835e8eb39cccf0522a4d045c34ca8ae0e3cdab
Vladimir Still <vl.still@gmail.com>
2014-03-20 17:46:29 +0100

    state: Add GoingToServe* states.

--- project/elevator/state.h
+++ project/elevator/state.h
@@ -20,6 +20,9 @@ enum class ChangeType {
     ButtonDownPressed,
     ButtonUpPressed,
 
+    GoingToServeUp,
+    GoingToServeDown,
+
     Served,
     ServedUp,
     ServedDown,
@@ -91,58 +94,6 @@ struct StateChange {
     ChangeType changeType;
     int changeFloor;
     ElevatorState state;
-};
-
-struct GlobalState {
-    void update( ElevatorState state ) {
-        Guard g{ _lock };
-        _elevators[ state.id ] = state;
-        updateButtons( g );
-    }
-
-    FloorSet upButtons() const { return _upButtons; }
-    FloorSet downButtons() const { return _downButtons; }
-    std::unordered_map< int, ElevatorState > elevators() const {
-        Guard g{ _lock };
-        return _elevators;
-    }
-
-    bool has( int i ) const {
-        Guard g{ _lock };
-        return _elevators.find( i ) != _elevators.end();
-    }
-
-    ElevatorState get( int i ) const {
-        Guard g{ _lock };
-        auto it = _elevators.find( i );
-        assert_neq( it, _elevators.end(), "state not found" );
-        return it->second;
-    }
-
-    void assertConsistency( const BasicDriverInfo &bi ) const {
-        Guard g{ _lock };
-        assert( _upButtons.consistent( bi ), "consistency check failed" );
-        assert( _upButtons.consistent( bi ), "consistency check failed" );
-        for ( auto &e : _elevators )
-            e.second.assertConsistency( bi );
-    }
-
-  private:
-    using Guard = std::unique_lock< std::mutex >;
-    mutable std::mutex _lock;
-    FloorSet _upButtons;
-    FloorSet _downButtons;
-    std::unordered_map< int, ElevatorState > _elevators;
-
-    void updateButtons( Guard & ) {
-        _upButtons.reset();
-        _downButtons.reset();
-        for ( const auto &el : _elevators ) {
-            _upButtons |= el.second.upButtons;
-            _downButtons |= el.second.downButtons;
-        }
-    }
-
 };
 
 }
